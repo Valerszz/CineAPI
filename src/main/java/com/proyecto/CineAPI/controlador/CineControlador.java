fe372ec5cfdb6cc18fd8f38cb3c08d05b32cbad1
marianav721 <flores.mariana@cetys.edu.mx>
2026-03-03 17:52:21 -0800

    hola controlador eliminar

--- src/main/java/com/proyecto/CineAPI/controlador/CineControlador.java
+++ src/main/java/com/proyecto/CineAPI/controlador/CineControlador.java
@@ -1,4 +1,14 @@
 package com.proyecto.CineAPI.controlador;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class CineControlador {
+    @DeleteMapping("{idL}")
+    public String deleteEstudiante(@PathVariable Long idL) {
+        return "Película eliminada";
+    }
+
+
+
 }
