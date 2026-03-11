e797459eebe1ec3b982fcdf9b028612ebbb67426
Valerszz <valeriaperez8a@gmail.com>
2026-03-10 17:40:41 -0700

    Crear Peliculas

--- src/main/java/com/proyecto/CineAPI/servicio/CineServicio.java
+++ src/main/java/com/proyecto/CineAPI/servicio/CineServicio.java
@@ -1,4 +1,21 @@
 package com.proyecto.CineAPI.servicio;
 
+import com.proyecto.CineAPI.model.Cine;
+import com.proyecto.CineAPI.repositorio.CineRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class CineServicio {
+    private CineRepositorio cineRepositorio;
+
+    @Autowired
+    public CineServicio(CineRepositorio cineRepositorio) {
+        this.cineRepositorio = cineRepositorio;
+    }
+    public List<Cine> crearPeliculas(Cine pelicula) {
+        return cineRepositorio.crearPeliculas(pelicula);
+    }
+
 }
